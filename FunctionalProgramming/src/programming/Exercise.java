ae88d246ff36f1cba372e3466a4e39aa9c314fc1
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-02-06 17:06:04 +0530

    Exercies

---
+++ FunctionalProgramming/src/programming/Exercise.java
@@ -0,0 +1,35 @@
+package programming;
+
+import java.util.List;
+
+public class Exercise {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 5, 4, 6, 44, 5, 12, 4);
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                "Kubernetes");
+
+        System.out.println("Printing the Odd Numbers:");
+        numbers.stream()
+                .filter(num -> num % 2 == 1)
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Printing all the Courses");
+        courses.stream()
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Printing Courses having word 'Spring' ");
+        courses.stream()
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Printing Courses having atleast 4 characters");
+        courses.stream()
+                .filter(course -> course.length()>=4)
+                .forEach(System.out::println);
+        System.out.println();
+
+    }
+}
--- FunctionalProgramming/src/programming/FP01Functional.java
+++ FunctionalProgramming/src/programming/FP01Functional.java
@@ -10,22 +10,12 @@ public class FP01Functional {
         printAllNumbersInListFunctional(numbers);
     }
 
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-
-    private static void print(int number) {
-        System.out.println(number);
-    }
-
     public static void printAllNumbersInListFunctional(List<Integer> numbers) {
         // Converting the list of numbers into Stream
 
         // numbers.stream().forEach(FP01Functional::/print); // Method Reference
         numbers.stream()
-               .filter(num->num%2 == 0) //Lambda Expression
-               .forEach(System.out::println);
-               
-        // .filter(FP01Functional::isEven) // Only filter the even numbers
+                .filter(num -> num % 2 == 0) // Lambda Expression
+                .forEach(System.out::println);
     }
 }
